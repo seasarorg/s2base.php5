727bb8e030397c835474b8d185e1f5d762e2046c
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2006-05-23 23:44:30 +0000

    add maple plugin add smarty plugin modify build.xml
    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/trunk@37 230b31e7-b2dc-410c-9553-f9f491091bec

---
+++ s2base.php5/vendor/plugins/maple/config/environment.inc.php
@@ -0,0 +1,12 @@
+<?php
+require_once('environment.inc.php');
+
+define('S2BASE_PHP5_PLUGIN_MAPLE',S2BASE_PHP5_ROOT . '/vendor/plugins/maple');
+
+/**
+ * cache switch
+define('S2AOP_PHP5_FILE_CACHE',true);
+define('S2AOP_PHP5_FILE_CACHE_DIR', S2BASE_PHP5_VAR_DIR . '/cache');
+define('S2CONTAINER_PHP5_CACHE_DIR',S2BASE_PHP5_VAR_DIR . '/cache');
+ */
+?>
---
+++ s2base.php5/vendor/plugins/maple/skeleton/dicon.php
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE components PUBLIC "-//SEASAR//DTD S2Container//EN"
+"http://www.seasar.org/dtd/components21.dtd">
+<components>
+    <!-- 
+    <include path="%S2BASE_PHP5_ROOT%/app/modules/ModuleName/dicon/ServiceName.dicon"/>
+    -->
+</components>
