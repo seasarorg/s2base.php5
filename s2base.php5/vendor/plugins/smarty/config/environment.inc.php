2669879163a6427bad0493ed104eb46821a16221
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2006-07-05 12:05:48 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/trunk@60 230b31e7-b2dc-410c-9553-f9f491091bec

--- s2base.php5/vendor/plugins/smarty/config/environment.inc.php
+++ s2base.php5/vendor/plugins/smarty/config/environment.inc.php
@@ -1,9 +1,4 @@
 <?php
-require_once('Smarty/libs/Smarty.class.php');
-require_once('S2Base/S2Base.web.php');
-
-define('S2BASE_PHP5_PLUGIN_SMARTY',S2BASE_PHP5_ROOT . '/vendor/plugins/smarty');
-
 /**
  * php log setting
  */
@@ -12,7 +7,18 @@ error_reporting(E_WARNING);
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 ini_set('log_errors','On');
-ini_set('error_log',S2BASE_PHP5_VAR_DIR . '/logs');
+ini_set('error_log',S2BASE_PHP5_VAR_DIR . '/logs/php.log');
+
+/**
+ * Library setting
+ */
+require_once('Smarty/libs/Smarty.class.php');
+require_once('S2Base/S2Base.web.php');
+
+/**
+ * Directory setting
+ */
+define('S2BASE_PHP5_PLUGIN_SMARTY',S2BASE_PHP5_ROOT . '/vendor/plugins/smarty');
 
 /**
  * session path setting
