bdbf124c7b41bfff6c995ddd54b027195e075f3e
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-03-21 03:54:58 +0000

    release-0.9
    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/branches/with-zf@254 230b31e7-b2dc-410c-9553-f9f491091bec

--- lib/S2Base/plugins/install.bat
+++ lib/S2Base/plugins/install.bat
@@ -1,7 +1,7 @@
 @echo off
 
-call pear install S2Base_Zf-0.8.0.tgz
-call pear install S2Base_Zf_Pager-0.8.0.tgz
-call pear install S2Base_Zf_Scaffold-0.8.0.tgz
+call pear install S2Base_Zf-0.9.0.tgz
+call pear install S2Base_Zf_Pager-0.9.0.tgz
+call pear install S2Base_Zf_Scaffold-0.9.0.tgz
 
 call pear list -c __uri
