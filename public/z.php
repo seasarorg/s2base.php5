3f7fc526a9d88717f9d77491f2a19f07b26d4422
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-03-25 03:06:04 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/trunk/s2base.php5@265 230b31e7-b2dc-410c-9553-f9f491091bec

---
+++ public/z.php
@@ -0,0 +1,31 @@
+<?php
+function microtime_float(){
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+} 
+$time_start = microtime_float();
+
+require_once(dirname(dirname(__FILE__)).'/config/environment.inc.php');
+require_once(dirname(dirname(__FILE__)).'/vendor/plugins/zf/config/environment.inc.php');
+
+try{
+    Zend_Session::regenerateId();
+    $request = new Zend_Controller_Request_Http();
+    $request->setBaseUrl();
+    $fc = Zend_Controller_Front::getInstance();
+    $fc->throwExceptions(true);
+    $fc->registerPlugin(new S2Base_ZfDispatcherSupportPlugin());
+    $validatePlugin = new S2Base_ZfValidateSupportPlugin();
+    $validatePlugin->addValidateFactory(new S2Base_ZfRegexValidateFactory());
+    $fc->registerPlugin($validatePlugin);
+    $fc->setDispatcher(new S2Base_ZfDispatcher());
+    $fc->setRequest($request);
+    $response = $fc->dispatch();
+}catch(Exception $e){
+    print '<pre><font color="red">' . $e->__toString() . '</font></pre>' . PHP_EOL;
+}
+
+$time_end = microtime_float();
+$time = $time_end - $time_start;
+echo "<br> [ dispatch time : $time seconds ] <br>" . PHP_EOL;
+?>
