c463602c771fc1433acd1dd82c1c1aeca458bc79
ueyama <ueyama@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-05-19 07:42:03 +0000

    Add PRADO plugin
    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/trunk@294 230b31e7-b2dc-410c-9553-f9f491091bec

---
+++ s2base.php5/vendor/plugins/prado/skeleton/module/application.xml.php
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<application id="@@MODULE_NAME@@">
+  <services>
+  	<service id="page" class="S2TPageService" DefaultPage="Home"/>
+  </services>
+</application>
