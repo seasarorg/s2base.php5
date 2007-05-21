00b3069296501b75c509ea109c1cb63b50256146
ueyama <ueyama@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-05-21 17:59:53 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/branches/with-prado@302 230b31e7-b2dc-410c-9553-f9f491091bec

---
+++ public/d.php
@@ -0,0 +1,23 @@
+<?php
+function microtime_float(){
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+} 
+$time_start = microtime_float();
+
+require_once(dirname(dirname(__FILE__)).'/config/environment.inc.php');
+require_once(dirname(dirname(__FILE__)).'/vendor/plugins/smarty/config/environment.inc.php');
+define('S2BASE_PHP5_REQUEST_MODULE_KEY','mod');
+define('S2BASE_PHP5_REQUEST_ACTION_KEY','act');
+define('S2BASE_PHP5_DEFAULT_MODULE_NAME','Default');
+define('S2BASE_PHP5_DEFAULT_ACTION_NAME','index');
+try{
+    S2Base_Dispatcher::dispatch(new S2Base_RequestImpl());
+}catch(Exception $e){
+    print "<pre><font color=\"red\">{$e->__toString()}</font></pre>\n";
+}
+
+$time_end = microtime_float();
+$time = $time_end - $time_start;
+echo "<br> [ dispatch time : $time seconds ] <br>\n";
+?>
