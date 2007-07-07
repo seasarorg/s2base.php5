4d185ccb3f525e8eaff04f94987d5ab1f077a8a9
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-07-07 09:21:36 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/trunk@332 230b31e7-b2dc-410c-9553-f9f491091bec

--- s2base.php5/public/z.php
+++ s2base.php5/public/z.php
@@ -1,22 +1,39 @@
 <?php
-function microtime_float(){
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
-} 
-$time_start = microtime_float();
+S2Base_ZfStopWatch::start();
 
 require_once(dirname(dirname(__FILE__)).'/config/environment.inc.php');
-require_once(dirname(dirname(__FILE__)).'/vendor/plugins/zf/config/environment.inc.php');
+require_once(dirname(dirname(__FILE__)).'/config/zf.inc.php');
 
 try{
     Zend_Session::regenerateId();
     S2Base_ZfInitialize::init();
     Zend_Controller_Front::getInstance()->dispatch();
 }catch(Exception $e){
-    print '<pre><font color="red">' . $e->__toString() . '</font></pre>' . PHP_EOL;
+    Zend_Registry::get('logger')->crit($e->__toString());
 }
 
-$time_end = microtime_float();
-$time = $time_end - $time_start;
-print "<br> [ dispatch time : $time seconds ] <br>" . PHP_EOL;
-?>
+Zend_Registry::get('logger')->info('dispatch time : ' . S2Base_ZfStopWatch::stop() . ' seconds');
+exit;
+
+class S2Base_ZfStopWatch {
+    private static $start = 0;
+    private static $place = 0;
+    private function __construct(){}
+    public static function start() {
+        self::$start = self::microtime_float();
+    }
+    public static function place($point = true) {
+        $point ? self::$place = self::microtime_float() : self::$place = 0;
+    }
+    public static function time() {
+        $start = self::$place === 0 ? self::$start : self::$place;
+        return self::microtime_float() - $start;
+    }
+    public static function stop() {
+        return self::microtime_float() - self::$start;
+    }
+    public static function microtime_float(){
+        list($usec, $sec) = explode(' ', microtime());
+        return ((float)$usec + (float)$sec);
+    }
+}
