70668bc62e4070c7b1c984b440f6a5c1699ae6b9
klove <klove@230b31e7-b2dc-410c-9553-f9f491091bec>
2007-10-02 12:48:05 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2base.php5/branches/s2base.php5-sandbox-2.0@392 230b31e7-b2dc-410c-9553-f9f491091bec

--- vendor/s2base/commands/AutoTestCommand.class.php
+++ vendor/s2base/commands/AutoTestCommand.class.php
@@ -1,8 +1,6 @@
 <?php
 class AutoTestCommand implements S2Base_GenerateCommand {
 
-    protected $testClasses = array();
-
     public function __construct() {
         $this->args = $_SERVER['argv'];
     }
@@ -33,37 +31,55 @@ class AutoTestCommand implements S2Base_GenerateCommand {
         $args = $_SERVER['argv'];
         while (true) {
             clearstatcache();
-            $this->testClasses = array();
-            $this->findTestClasses($testDir);
-            $monitorTmp = array();
-            foreach($this->testClasses as $testClass => $testFile) {
-                $monitorTmp[$testFile] = filemtime($testFile);
+            $testClasses = array();
+            $testClasses = $this->findTestClasses($testDir, $testClasses);
+            $addFiles = array_diff(array_values($testClasses), array_keys($monitorFiles));
+            $delFiles = array_diff(array_keys($monitorFiles), array_values($testClasses));
+
+            foreach ($addFiles as $testFile) {
+                print "[INFO ] test add : $testFile" . PHP_EOL;
+                $stamp = filemtime($testFile);
+                $testStamp = $isFirst ? $stamp : $stamp -1;
+                $testClass = array_search($testFile, $testClasses, true);
+
+                $srcClass = preg_replace('/Test$/', '', $testClass);
+                $srcFile = $testFile;
+                $srcFile = str_replace($testClass . S2BASE_PHP5_CLASS_SUFFIX,
+                                       $srcClass  . S2BASE_PHP5_CLASS_SUFFIX, $srcFile);
+                $srcFile = str_replace(S2BASE_PHP5_ROOT . DIRECTORY_SEPARATOR . 'test',
+                                       S2BASE_PHP5_ROOT . DIRECTORY_SEPARATOR . 'app', $srcFile);
+                if (file_exists($srcFile)) {
+                    $srcStamp = filemtime($srcFile);
+                    $srcStamp = $isFirst ? $srcStamp : $srcStamp -1;
+                } else {
+                    $srcFile = null;
+                    $srcStamp = null;
+                }
+                $monitorFiles[$testFile] = array('test_stamp' => $testStamp,
+                                                 'test_class' => $testClass,
+                                                 'src_file'   => $srcFile,
+                                                 'src_stamp'  => $srcStamp,
+                                                 'src_class'  => $srcClass);
             }
-            $addFiles = array_diff_key($monitorTmp, $monitorFiles);
-            $delFiles = array_diff_key($monitorFiles, $monitorTmp);
-            foreach ($addFiles as $file => $stamp) {
-                print "[INFO ] test add : $file" . PHP_EOL;
-                $monitorFiles[$file] = $isFirst ? $stamp : $stamp -1;
+
+            foreach ($delFiles as $testFile) {
+                print "[INFO ] test del : $testFile" . PHP_EOL;
+                unset($monitorFiles[$testFile]);
             }
-            foreach ($delFiles as $file => $stamp) {
-                print "[INFO ] test del : $file" . PHP_EOL;
-                unset($monitorFiles[$file]);
-            }
-            foreach ($monitorFiles as $testFile => $stamp) {
-                $currentStamp = filemtime($testFile);
-                if ($currentStamp > $stamp) {
-                    $monitorFiles[$testFile] = $currentStamp;
-                    $testTarget = str_replace(S2BASE_PHP5_ROOT . DIRECTORY_SEPARATOR, '', $testFile);
-                    $testTarget = str_replace(DIRECTORY_SEPARATOR, '.', $testTarget);
-                    if (preg_match('/\.php$/', $this->args[0])) {
-                        $cmd = "php {$this->args[0]} test $testTarget";
-                    } else {
-                        $cmd = "phing test -Dtt=$testTarget";
-                    }
+
+            foreach ($monitorFiles as $testFile => $testInfo) {
+                $testStamp = filemtime($testFile);
+                if ($testStamp > $testInfo['test_stamp']) {
+                    $monitorFiles[$testFile]['test_stamp'] = $testStamp;
                     print PHP_EOL . "[INFO ] modified : $testFile" . PHP_EOL;
-                    print "[INFO ] execute  : $cmd" . PHP_EOL;
-                    system($cmd);
-                    print PHP_EOL . PHP_EOL . '[INFO ] monitoring test files. . . . .' . PHP_EOL;
+                    $this->executeTest($testFile);
+                } else if ($testInfo['src_file'] !== null) {
+                    $srcStamp = filemtime($testInfo['src_file']);
+                    if ($srcStamp > $testInfo['src_stamp']) {
+                        $monitorFiles[$testFile]['src_stamp'] = $srcStamp;
+                        print PHP_EOL . "[INFO ] modified : $srcFile" . PHP_EOL;
+                        $this->executeTest($testFile);
+                    }
                 }
             }
             sleep(2);
@@ -71,7 +87,20 @@ class AutoTestCommand implements S2Base_GenerateCommand {
         }
     }
 
-    protected function findTestClasses($root) {
+    protected function executeTest($testFile) {
+        $testTarget = str_replace(S2BASE_PHP5_ROOT . DIRECTORY_SEPARATOR, '', $testFile);
+        $testTarget = str_replace(DIRECTORY_SEPARATOR, '.', $testTarget);
+        if (preg_match('/\.php$/', $this->args[0])) {
+            $cmd = "php {$this->args[0]} test $testTarget";
+        } else {
+            $cmd = "phing test -Dtt=$testTarget";
+        }
+        print "[INFO ] execute  : $cmd" . PHP_EOL;
+        system($cmd);
+        print PHP_EOL . PHP_EOL . '[INFO ] monitoring test files. . . . .' . PHP_EOL;
+    }
+
+    protected function findTestClasses($root, &$spool) {
         $items = scandir($root);
         if ($items === false) {
             print "[ERROR] scan directory [$root] failed." . PHP_EOL;
@@ -84,12 +113,12 @@ class AutoTestCommand implements S2Base_GenerateCommand {
             }
             $rootItem  = $root  . DIRECTORY_SEPARATOR . $item;
             if (is_dir($rootItem)) {
-                $this->findTestClasses($rootItem);
+                $spool = array_merge($spool, $this->findTestClasses($rootItem, $spool));
             }
             else if (is_file($rootItem)) {
                 $matches = array();
-                if (preg_match('/(.+?Test)\..*php/', $item, $matches)) {
-                    $this->testClasses[$matches[1]] = $rootItem;
+                if (preg_match('/(.+?Test)\..*php$/', $item, $matches)) {
+                    $spool[$matches[1]] = $rootItem;
                 }
             }
             else {
@@ -97,5 +126,7 @@ class AutoTestCommand implements S2Base_GenerateCommand {
                 exit;
             }
         }
+        return $spool;
     }
 }
+
